2fc6942cf58e60e40db3e55a2335b32f67880d0b
xola-mthembu <xola.mthembu@gmail.com>
2024-03-12 07:43:13 -0700

    Task 11 updated

--- console.py
+++ console.py
@@ -30,13 +30,11 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def default(self, line):
-        """Default behavior for unknown commands."""
+        """Default behavior for <class name>.<command> format."""
         try:
-            if '.' in line and '(' in line and ')' in line:
-                cls_name, method = line.split(".", 1)
-                method_name = method.split("(", 1)[0]
-                if method_name == "all":
-                    self.do_all(cls_name)
+            cls_name, command = line.split(".", 1)
+            if command == "all()":
+                self.do_all(cls_name)
             else:
                 print("*** Unknown syntax:", line)
         except Exception as e:
@@ -45,20 +43,17 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, arg):
         """Prints all instances based or not on the class name."""
         all_objs = storage.all()
-        if not arg:
-            print([str(obj) for obj in all_objs.values()])
-        elif arg in HBNBCommand.class_dict:
-            relevant_objs = [obj for obj in all_objs.values() if
-                             type(obj).__name__ == arg]
-            print([str(obj) for obj in relevant_objs])
+        if not arg or arg in HBNBCommand.class_dict:
+            filtered_objs = [str(obj) for obj in all_objs.values()
+                             if not arg or type(obj).__name__ == arg]
+            print(filtered_objs)
         else:
             print("** class doesn't exist **")
 
     # Include other command methods like do_create, do_show, etc.
 
+
 # Dictionary to map class names to classes
-
-
 HBNBCommand.class_dict = {
     'BaseModel': BaseModel,
     'User': User,
